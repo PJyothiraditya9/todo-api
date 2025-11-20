9be63ffb4caefe749e563f7e5187888eb79ee96d
Manu <paindlamani@gmail.com>
2025-11-20 13:33:17 +0530

    waste

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const FILE = "/data/todos.json";
+const FILE = "./todos.json";
 
 function loadTodos() {
   if (!fs.existsSync(FILE)) {
