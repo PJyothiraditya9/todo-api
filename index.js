a596fdcb6dfc63354d90f38d519b2868f78f5e55
Manu <paindlamani@gmail.com>
2025-11-20 13:44:30 +0530

    final

--- index.js
+++ index.js
@@ -87,5 +87,5 @@ app.delete("/todos/:id", (req, res) => {
   res.status(200).json({ message: "Todo deleted" });
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
